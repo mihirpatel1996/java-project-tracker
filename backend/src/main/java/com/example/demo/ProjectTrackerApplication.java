fa69c80fc6dea3c08b00093b88480d246010db6c
mihirpatel1996 <mihirpatel4798@gmail.com>
2025-12-23 17:14:22 -0400

    frontend added

---
+++ backend/src/main/java/com/example/demo/ProjectTrackerApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ProjectTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackerApplication.class, args);
+	}
+
+	//	Global configuration of CORS
+	@Bean
+	public WebMvcConfigurer corsConfirgurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/")
+						.allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
+}
